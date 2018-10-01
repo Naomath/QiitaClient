ffec961a7b8e7777ce494b9644c16c3d7853539f
naomath <math1972naonao@gmail.com>
2018-10-01 14:20:22 +0900

    Initial commit

---
+++ app/src/main/kotlin/com/naoto/dennnoukishidann/qittaclient/model/User.kt
@@ -0,0 +1,8 @@
+package com.naoto.dennnoukishidann.qittaclient.model
+
+/**
+ * Created by gotounaoto on 2018/10/01.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl:String)
